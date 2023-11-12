3f221a6b3953a5bf6d37f4b95e7a32477489bba1
GrantLicata <grant.licata@gmail.com>
2023-11-11 19:33:52 -0800

    Explicit function typing

--- app/functions/page.ts
+++ app/functions/page.ts
@@ -4,3 +4,10 @@
 let sayHi = () => {
   console.log("Hi, welcome to this section");
 };
+sayHi = "hi";
+
+// Functions can be explicitly typed following the arguments
+let funcReturnString = (): string => {
+  console.log("Hi");
+  return "This is a string";
+};
